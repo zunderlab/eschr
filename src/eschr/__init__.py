e52df05d3a184b8b89f27f80512db504bd111b8f
Sarah Goggin <35312830+smgoggin10@users.noreply.github.com>
2024-03-28 10:13:36 -0400

    clean up __init__.py

--- src/eschr/__init__.py
+++ src/eschr/__init__.py
@@ -1,18 +1,7 @@
 from importlib.metadata import version
 
-#from eschr.read_write_utils import csv_to_zarr, make_zarr
-#from eschr.plotting import smm_heatmap, umap
-#from eschr import pl, tl
-#from eschr import tl
-from . import tl, readwrite
-
-
-#from __future__ import absolute_import
-#import eschr._read_write_utils
-#import eschr.consensus_cluster
-#import eschr.pl
+from . import pl, tl, readwrite
 
 __all__ = ["pl", "tl", "readwrite"]
 
-#from .version import __version__
 __version__ = version("eschr")
