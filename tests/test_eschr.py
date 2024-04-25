1b33c8920ccab4388129953fb46e01ed246d8a99
Sarah Goggin <35312830+smgoggin10@users.noreply.github.com>
2024-04-25 15:42:44 -0400

    add tests for plotting

--- tests/test_eschr.py
+++ tests/test_eschr.py
@@ -1,6 +1,6 @@
 import pytest
 
-import eschr
+import eschr as es
 
 import anndata
 import numpy as np
@@ -15,3 +15,69 @@ def test_package_has_version():
 @pytest.mark.skip(reason="This decorator should be removed when test passes.")
 def test_example():
     assert 1 == 0  # This test is designed to fail.
+
+@pytest.fixture
+def adata():
+    data = pd.read_csv("data/test_data.csv", index_col=0).to_numpy()
+    adata = anndata.AnnData(X=data)
+    return adata
+
+# TEST TOOL FUNCTIONS
+
+
+# TEST PLOTTING FUNCTIONS
+def test_smm_heatmap_default(adata):
+    # Test with default parameters
+    es.pl.smm_heatmap(adata)
+
+def test_smm_heatmap_custom(adata):
+    # Test with custom parameters
+    es.pl.smm_heatmap(adata, smm_cmap='viridis', feat_cmap='magma', 
+                      show=False, output_path='heatmap.png')
+
+def test_smm_heatmap_invalid_output_path(adata):
+    # Test exception with invalid output path
+    with pytest.raises(Exception):
+        es.pl.smm_heatmap(adata, output_path='invalid/path')
+
+def test_min_max_scaler_default():
+    data = [1, 2, 3, 4, 5]
+    
+    # Test default min and max
+    scaled = es.pl.min_max_scaler(data)
+    assert np.isclose(scaled[0], 0)
+    assert np.isclose(scaled[-1], 1)
+
+def test_slanted_orders():
+    data = [[1, 2], [3, 4]]
+    
+    # Test default parameters
+    row_order, col_order = es.pl.slanted_orders(data)
+    assert row_order.tolist() == [1, 0]
+    assert col_order.tolist() == [1, 0]
+
+    # Test order rows only
+    row_order, col_order = es.pl.slanted_orders(data, order_cols=False)
+    assert row_order.tolist() == [1, 0]
+    assert col_order.tolist() == [0, 1]
+
+    # Test order cols only
+    row_order, col_order = es.pl.slanted_orders(data, order_rows=False) 
+    assert row_order.tolist() == [0, 1]
+    assert col_order.tolist() == [1, 0]
+
+    # Test no ordering
+    row_order, col_order = es.pl.slanted_orders(data, order_rows=False, order_cols=False)
+    assert row_order.tolist() == [0, 1]
+    assert col_order.tolist() == [0, 1]
+
+def test_run_umap_default(adata):
+    # Test default return 
+    plotting.run_umap(adata)
+    assert 'X_umap' in adata.obsm
+
+def test_run_umap_return_layout(adata):
+    # Test return layout
+    embedding = plotting.run_umap(adata, return_layout=True)
+    assert isinstance(embedding, np.ndarray)
+    assert embedding.shape[1] == 2
