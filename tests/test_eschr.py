7d58a71d320a350f6e701296dcada03663b60998
Sarah Goggin <35312830+smgoggin10@users.noreply.github.com>
2024-08-27 17:27:22 -0400

    fix cc test

--- tests/test_eschr.py
+++ tests/test_eschr.py
@@ -213,7 +213,7 @@ def test_get_hard_soft_clusters_single_cluster(setup_data):
 # consensus_cluster_leiden
 def test_consensus_cluster_leiden(bipartite_graph_array):
     bipartite = bipartite_graph_array
-    n = np.max(bipartite_graph_array)+1
+    n = np.max(bipartite_graph_array.row)+1
     in_args = (n, 1.0, bipartite)
     hard_clusters, soft_membership_matrix, resolution = es.tl.clustering.consensus_cluster_leiden(in_args)
 
