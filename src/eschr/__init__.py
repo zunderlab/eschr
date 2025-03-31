b7f48ed572a6fd587e3aa47b7d23e696b2974511
Sarah Goggin <35312830+smgoggin10@users.noreply.github.com>
2025-03-31 11:59:45 -0400

    remove readwrite

--- src/eschr/__init__.py
+++ src/eschr/__init__.py
@@ -1,7 +1,7 @@
 from importlib.metadata import version
 
-from . import pl, readwrite, tl
+from . import pl, tl
 
-__all__ = ["pl", "tl", "readwrite"]
+__all__ = ["pl", "tl"]
 
 __version__ = version("eschr")
